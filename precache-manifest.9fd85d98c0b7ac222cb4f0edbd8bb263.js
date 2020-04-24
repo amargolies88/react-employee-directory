b444561f35a40f134ba8ee188c8e7a24946094a2
Alan Margolies <amargolies88@gmail.com>
2020-04-24 12:57:11 -0400

    Updates

---
+++ precache-manifest.9fd85d98c0b7ac222cb4f0edbd8bb263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97d298148e95d81a00684ae0b893113",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "5693612b3aa33e44e1f4",
+    "url": "/react-employee-directory/static/css/main.f088ca48.chunk.css"
+  },
+  {
+    "revision": "67f984c51283a8691245",
+    "url": "/react-employee-directory/static/js/2.1a9bfc48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.1a9bfc48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5693612b3aa33e44e1f4",
+    "url": "/react-employee-directory/static/js/main.02c47b26.chunk.js"
+  },
+  {
+    "revision": "a1dc9396c4eb2e1640c7",
+    "url": "/react-employee-directory/static/js/runtime-main.8a05b047.js"
+  }
+]);
